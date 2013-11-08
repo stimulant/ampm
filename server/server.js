acf2f7d7083f0d1a6c749a4f046fd8d4bc09f360
Josh Santangelo <josh@stimulant.io>
2013-11-07 18:56:49 -0800

    playing

--- server/server.js
+++ server/server.js
@@ -2,63 +2,85 @@ var express = require('express');
 var request = require('request');
 var http = require('http');
 var fs = require('fs');
-var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
 var path = require('path');
 var _ = require('underscore');
 var XRegExp = require('xregexp').XRegExp;
 var moment = require('moment');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
-var config = require('./config.js').config;
 server.listen(3000);
 
-request(config.rootXml, function(error, response, body) {
-    response.body = null;
+// Load config file.
+var config = require('./config.js').config;
 
-    var pattern = new XRegExp(/>(http.*?)</gi);
-    var matches = [];
-    var match;
+updateContent();
 
-    while (true) {
-        match = pattern.exec(body);
-        if (!match) {
-            break;
-        }
-        matches.push(match);
+function updateContent() {
+    if (!fs.existsSync(config.outputBase)) {
+        fs.mkdirSync(config.outputBase);
     }
 
-    pattern = new XRegExp(config.cmsRoot + '/(.*)');
-    _.each(matches, function(element, index, list) {
-        var url = element[1];
+    // Load the root content XML file.
+    request(config.rootXml, function(error, response, body) {
 
-        var filepath = config.outputBase + XRegExp.exec(url, pattern)[1];
-        var dir = path.dirname(filepath);
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
+        // Collect any URLs in root file.
+        var pattern = new XRegExp(/>(http.*?)</gi);
+        var matches = [];
+        var match;
+
+        while (true) {
+            match = pattern.exec(body);
+            if (!match) {
+                break;
+            }
+            matches.push(match);
         }
 
-        if (fs.existsSync(filepath)) {
-            var localFileModified = moment(fs.statSync(filepath).mtime);
-            request({
-                url: url,
-                method: 'HEAD'
-            }, function(error, response, body) {
-                fs.writeFile('response.json', JSON.stringify(response, null, 4));
-                var remoteFileModified = moment(response.headers['last-modified']);
-                if (!remoteFileModified.isAfter(localFileModified)) {
-                    console.log('Skipping ' + url);
-                } else {
-                    console.log('Downloading ' + url);
-                    request(url, function(error, response, body) {
+        pattern = new XRegExp(config.cmsRoot + '/(.*)');
+        _.each(matches, function(element, index, list) {
+            var url = element[1];
+            var filepath = config.outputBase + XRegExp.exec(url, pattern)[1];
 
-                    }).pipe(fs.createWriteStream(filepath));
-                }
-            });
-        }
-    });
-}).pipe(fs.createWriteStream(config.outputBase + 'content.xml'));
+            // Check if the file has been downloaded already.
+            if (fs.existsSync(filepath)) {
+                var localFileModified = moment(fs.statSync(filepath).mtime);
 
+                // If it has, see if the one on the server is newer.
+                request({
+                    url: url,
+                    method: 'HEAD'
+                }, function(error, response, body) {
+                    var remoteFileModified = moment(response.headers['last-modified']);
+                    if (remoteFileModified.isAfter(localFileModified)) {
+                        downloadLinkedFile(url, filepath);
+                    } else {
+                        console.log('Skipping ' + url);
+                    }
+                });
+            } else {
+                downloadLinkedFile(url, filepath);
+            }
+        });
+    }).pipe(fs.createWriteStream(config.outputBase + 'content.xml'));
+}
+
+// Download a file to a given path on disk.
+
+function downloadLinkedFile(url, filepath) {
+    console.log('Downloading ' + url);
+
+    var dir = path.dirname(filepath);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+
+    request(url, function(error, response, body) {
+        console.log('Error loading ' + url);
+        console.log(error);
+    }).pipe(fs.createWriteStream(filepath));
+}
 
 // routing......
 app.get('/', function(req, res) {
