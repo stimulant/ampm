c1857856e3716f17006939113154212e444519a4
Josh Santangelo <josh@stimulant.io>
2013-11-08 17:09:17 -0800

    todo list

--- server/server.js
+++ server/server.js
@@ -6,6 +6,45 @@ var io = require('socket.io').listen(server);
 
 server.listen(3000);
 
+///// Support multiple clients
+// Each client connects with a config, including its network path for updating content
+
+///// Updater
+// save to temp folder and then copy over
+// update currently loading file(s?), progress of each, total progress, complete event
+// Add app to content, unzip
+// Support to update from non-web location
+
+///// App controller
+// Monitor process
+// Restart on hang
+// Give up restart after n times
+
+///// Server
+// Schedule content update (shut down before, restart after)
+// Schedule shutdown/restart
+// Run as service? https://npmjs.org/package/node-windows
+
+///// Logger
+// Server listens to events from updater/controller and sends those to logger
+// Logger listens on a port for log messages
+
+///// Monitor
+// Monitor listens on a port for monitor messages
+// Revisit ICE for patterns
+
+///// UI
+// shut down / start (toggle)
+// update (show progress)
+// Display monitor status
+// Display recent logs
+// Memory/CPU usage https://github.com/markitondemand/node-perfmon
+
+///// Central UI
+// Forward all messages to another instance of server?
+// UI displays all inputs at once?
+// Send commands (shutdown etc) back to clients?
+
 // Load config file.
 var config = require('./config.js').config;
 var Updater = require('./updater.js').Updater;
