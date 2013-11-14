7751947525bd4ae695fbf348f294a5e8adb0c019
Josh Santangelo <josh@stimulant.io>
2013-11-13 16:19:51 -0800

    request app state in a loop, not on a timer

--- testapp/Client/MainWindow.xaml.cs
+++ testapp/Client/MainWindow.xaml.cs
@@ -26,17 +26,48 @@ namespace Client
             Server.ConsumeParsingExceptions = false;
             Server.MessageReceived += Server_MessageReceived;
             Server.Start();
+
+            Loaded += MainWindow_Loaded;
         }
 
+        /// <summary>
+        /// Send a heartbeat every frame.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void Timer_Tick(object sender, EventArgs e)
         {
             OscMessage message = new OscMessage(Source, "/heart/" + DateTime.Now.Millisecond);
             message.Send(Destination);
+        }
 
-            message = new OscMessage(Source, "/getAppState/");
+        /// <summary>
+        /// Get the initial app state.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            OscMessage message = new OscMessage(Source, "/getAppState/");
             message.Send(Destination);
         }
 
+        /// <summary>
+        /// Send state changes to the server.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            Point position = e.GetPosition(this);
+            new OscMessage(Source, "/" + string.Join("/", "mouse", "x", position.X, "y", position.Y)).Send(Destination);
+            base.OnMouseMove(e);
+        }
+
+        /// <summary>
+        /// Whenever a message is received, parse it and pass it to the UI thread.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void Server_MessageReceived(object sender, OscMessageReceivedEventArgs e)
         {
             string[] parts = e.Message.Address.Substring(1).Split(new char[] { '/' }, 2, StringSplitOptions.RemoveEmptyEntries);
@@ -46,16 +77,16 @@ namespace Client
             Dispatcher.BeginInvoke((Action)(() => HandleMessage(token)), DispatcherPriority.Input);
         }
 
+        /// <summary>
+        /// Do something with the message and then request state again.
+        /// </summary>
+        /// <param name="token"></param>
         private void HandleMessage(dynamic token)
         {
             _Cursor.RenderTransform = new TranslateTransform { X = token.attrs.x - _Cursor.Width / 2, Y = token.attrs.y - _Cursor.Height / 2 };
-        }
 
-        protected override void OnMouseMove(MouseEventArgs e)
-        {
-            Point position = e.GetPosition(this);
-            new OscMessage(Source, "/" + string.Join("/", "mouse", "x", position.X, "y", position.Y)).Send(Destination);
-            base.OnMouseMove(e);
+            OscMessage message = new OscMessage(Source, "/getAppState/");
+            message.Send(Destination);
         }
     }
 }
