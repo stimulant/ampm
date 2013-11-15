1701883c327c8e0c63c9a627e438883bfaefdb91
unknown <josh@DakutenVM.stimulant.local>
2013-11-14 21:41:18 -0800

    todo

--- server/server.js
+++ server/server.js
@@ -92,52 +92,128 @@ oscReceive.on('getServerState', function(message, sender) {
         sender.send('/serverState/' + JSON.stringify(serverState.xport()));
     }, throttle);
 });
+var express = require('express'); // Routing framework. http://expressjs.com/
+var http = require('http'); // HTTP support. http://nodejs.org/api/http.html
+var fs = require('node-fs'); // Recursive directory creation. https://github.com/bpedro/node-fs
+var osc = require('node-osc'); // OSC server. https://github.com/TheAlphaNerd/node-osc
+var _ = require('underscore'); // Utilities. http://underscorejs.org/
+var Backbone = require('backbone'); // Data model utilities. http://backbonejs.org/
 
-///// Support multiple clients
-// Each client connects with a config, including its network path for updating content
+// Load config file.
+try {
+    global.config = JSON.parse(fs.readFileSync('./config.json'));
+} catch (error) {
+    console.log("Couldn't load config file.");
+    console.log(error);
+}
 
-///// Updater
-// Kill/start button
-// Update button: kill process, update content, update client, restart client
-// Update server
-// Support to update from non-web location
+// Set up server.
+var app = express();
+var server = http.createServer(app);
+global.io = require('socket.io').listen(server);
+io.set('log level', 2);
+server.listen(3000);
 
-///// App controller
-// Monitor process
-// Restart on hang
-// Give up restart after n times
+// Set up OSC server to receive messages from app.
+global.oscServer = new osc.Server(3001);
+global.oscClient = new osc.Client('127.0.0.1', 3002);
+oscServer.on('message', function(msg, rinfo) {
+    // Convert OSC messages to objects and emit them similar to sockets.
+    var parts = msg[0].substr(1).split('/');
+    var action = parts.shift();
 
-///// Server
-// Schedule content update (shut down before, restart after)
-// Schedule shutdown/restart
-// Run as service? https://npmjs.org/package/node-windows
+    var message = {};
+    while (parts.length) {
+        var key = parts.shift();
+        var val = parts.shift();
+        var f = parseFloat(val);
+        message[key] = isNaN(f) ? val : f;
+    }
 
-///// Logger
-// Server listens to events from updater/controller and sends those to logger
-// Logger listens on a port for log messages
+    oscServer.emit(action, message);
+});
 
-///// Monitor
-// Monitor listens on a port for monitor messages
-// Revisit ICE for patterns
+// Set up view routing.
+app.use('/static', express.static(__dirname + '/view'));
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/view/index.html');
+});
 
-///// UI
-// shut down / start (toggle)
-// update (show progress)
-// Display monitor status
-// Display recent logs
-// Memory/CPU usage https://github.com/markitondemand/node-perfmon
+// Set up models.
+var ServerState = require('./model/serverState.js').ServerState;
+var serverState = new ServerState();
 
-///// Central UI
-// Forward all messages to another instance of server?
-// UI displays all inputs at once?
-// Send commands (shutdown etc) back to clients?
+var AppState = require('./model/appState.js').AppState;
+var appState = new AppState();
 
-///// Plugin for custom app logic
-// Short term -- extend serverState class
-// Long term -- define a set of properties/types/intervals to keep in sync across clients.
+// Update clients with server state when they ask for it, throttled to 60 FPS.
+var throttle = 1000 / 60;
+io.sockets.on('connection', function(socket) {
+    socket.on('getServerState', function(message) {
+        clearInterval(socket.serverInterval);
+        socket.serverInterval = setInterval(function() {
+            socket.emit('serverState', serverState.xport());
+        }, throttle);
+    });
 
-///// Analytics
-// Hook into analytics service? Or log analysis tool?
+    socket.on('getAppState', function(message) {
+        clearInterval(socket.appInterval);
+        socket.appInterval = setInterval(function() {
+            socket.emit('appState', appState.xport());
+        }, throttle);
+    });
+});
 
-///// V.next
-// Try to reduce export payload
+oscServer.on('getAppState', function(message) {
+    clearInterval(oscClient.appInterval);
+    oscClient.appInterval = setInterval(function() {
+        oscClient.send('/appState/' + JSON.stringify(appState.xport()));
+    }, throttle);
+});
+
+oscServer.on('getServerState', function(message) {
+    clearInterval(oscClient.serverInterval);
+    oscClient.serverInterval = setInterval(function() {
+        oscClient.send('/serverState/' + JSON.stringify(serverState.xport()));
+    }, throttle);
+});
+/*
+Client
+    Stop ignoring local updates when master disappears
+    Better sample -- each client has its own state, but reflects state of others
+
+Server
+    Logging
+        Log content updates
+        Log on request from client
+        Email on critical state
+        Analytics service?
+    Scheduling
+        Schedule content update
+        Schedule shutdown, startup, restart
+    Persistence
+        Monitor uptime
+        Restart on hang/crash
+        Give up restart after n times
+    Try to reduce message payload size
+    Run as service? https://npmjs.org/package/node-windows
+
+Console
+    Output
+        Number of clients
+        Is client running
+        Memory/CPU usage https://github.com/markitondemand/node-perfmon
+        Recent logs
+        Display arbitrary amount of state per client (like ICE)
+    Input
+        Kill running client
+        Kill all running clients
+        Start dead client
+        Start all clients
+        Restart client
+        Restart all clients
+        Update content on client: kill process, update content, update client, restart client
+        Update content on all clients: kill process, update content, update client, restart client
+        Update master server
+        Update client servers
+*/
