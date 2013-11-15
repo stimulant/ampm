19fcc5ffd2801a7f6bbcb75c3c8ba803e68999e2
Josh Santangelo <josh@stimulant.io>
2013-11-14 17:31:34 -0800

    committing modules, since they can't be built on machines without VS

--- server/start.ps1
+++ server/start.ps1
@@ -1,3 +1,7 @@
+# Ideally, this would:
+# Install git (and add to path) https://msysgit.googlecode.com/files/Git-1.8.4-preview20130916.exe
+# Install node (and add to path) http://nodejs.org/dist/v0.10.22/x64/node-v0.10.22-x64.msi
+
 # Install any node modules
 npm install
 
