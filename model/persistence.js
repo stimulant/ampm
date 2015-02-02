95ee87bc23adc3c068823d8e69455fb14b3bca5e
Josh Santangelo <josh@stimulant.com>
2015-02-02 15:45:31 -0800

    debugging some mysterious shutdowns

--- model/persistence.js
+++ model/persistence.js
@@ -294,6 +294,8 @@ exports.Persistence = BaseModel.extend({
             return;
         }
 
+        logger.info(new Error().stack);
+
         this._isShuttingDown = true;
 
         // See if the app is running.
