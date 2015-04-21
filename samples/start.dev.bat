030f047cf500c415892936f73dbe6e8dc74306a2
Josh Santangelo <josh@stimulant.com>
2015-04-21 14:18:13 -0700

    delete samples, use other repo instead

--- samples/start.dev.bat
+++
@@ -1,8 +0,0 @@
-:: Run ampm from its repo, assume everything is installed, and watch everything.
-CD ..\..\ampm
-supervisor ^
-	--watch .,..\ampm-test\wpf-test\config.json,restart.json ^
-	--ignore .git,node_modules,view,samples,logs,app,content,state.json ^
-	--extensions js,json ^
-	--no-restart-on error ^
-	-- server.js ..\ampm-test\wpf-test\config.json dev
