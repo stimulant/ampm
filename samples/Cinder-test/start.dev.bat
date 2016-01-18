06942446e4da13ea28e8157c5d18ae55392efb36
Josh Santangelo <josh@endquote.com>
2016-01-17 19:15:04 -0800

    remove cinder sample until it's updated

--- samples/Cinder-test/start.dev.bat
+++
@@ -1,8 +0,0 @@
-:: Run ampm from its repo, assume everything is installed, and watch everything.
-CD ..\..\ampm
-supervisor ^
-	--watch .,..\ampm-samples\cinder-test\ampm.json,..\ampm-samples\sharedState.js,restart.json ^
-	--ignore .git,node_modules,view,samples,logs,app,content,state.json ^
-	--extensions js,json ^
-	--no-restart-on error ^
-	-- server.js ..\ampm-samples\cinder-test\ampm.json dev.foo.bar
