830ef3bed6e92b0f22d4bbecd30d05050182fcd6
Josh Santangelo <josh@stimulant.com>
2016-01-23 15:42:12 -0800

    fix arguments being passed by nodemon

--- start.js
+++ start.js
@@ -5,13 +5,13 @@
 var nodemon = require('nodemon');
 var path = require('path');
 
-var appPath = path.resolve(path.dirname(configFile));
+var appPath = path.resolve(path.dirname(process.argv[2]));
 
 function start() {
     nodemon({
         script: path.join(__dirname, 'server.js'), // Run ampm's server.js
         verbose: true, // verbose doesn't seem to work?
-        args: process.argv, // Pass arguments on to ampm
+        args: process.argv.slice(2), // Pass arguments on to ampm
         watch: [
             path.join(appPath, 'restart.json'), // Restart when ampm tries to tigger a restart itself.
             __dirname // Restart when ampm code changes, for doing development.
