61345db4fca76ec0fa48456e00578abd294500a7
Sorob Louie <sorob@sorob.com>
2017-05-25 12:47:05 -0700

    there are cors issues with fetch

--- model/network.js
+++ model/network.js
@@ -114,6 +114,7 @@ exports.Network = BaseModel.extend({
         // Send the config on a request to /config from anywhere.
         app.get('/config', function(req, res) {
             res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
             res.header('Access-Control-Allow-Methods', 'GET');
             res.header('Access-Control-Allow-Headers', 'Content-Type');
             res.json($$consoleState.fullConfig());
